4d341a0b9852ba54ee68e6f2e58ce1b47d436710
길재현 <wogus4048@naver.com>
2024-07-16 18:56:45 +0900

    chore: before merge

--- backend/server/src/main/java/shop/sellution/server/order/application/OrderCreationService.java
+++ backend/server/src/main/java/shop/sellution/server/order/application/OrderCreationService.java
@@ -4,10 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.sellution.server.address.domain.AddressRepository;
+import shop.sellution.server.company.domain.Company;
 import shop.sellution.server.company.domain.CompanyRepository;
+import shop.sellution.server.customer.domain.Customer;
 import shop.sellution.server.customer.domain.CustomerRepository;
+import shop.sellution.server.global.exception.BadRequestException;
+import shop.sellution.server.global.exception.ExceptionCode;
 import shop.sellution.server.order.domain.Order;
 import shop.sellution.server.order.dto.request.SaveOrderReq;
+
+import static shop.sellution.server.global.exception.ExceptionCode.NOT_FOUND_COMPANY_ID;
+import static shop.sellution.server.global.exception.ExceptionCode.NOT_FOUND_CUSTOMER;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +29,12 @@ public class OrderCreationService {
 
     @Transactional
     public Order createOrder(SaveOrderReq saveOrderReq){
+        Company company = companyRepository.findById(saveOrderReq.getCompanyId())
+                .orElseThrow(() -> new BadRequestException(NOT_FOUND_COMPANY_ID));
+        Customer customer = customerRepository.findById(saveOrderReq.getCustomerId())
+                .orElseThrow(() -> new BadRequestException(NOT_FOUND_CUSTOMER));
+
+
 
         return null;
     }
