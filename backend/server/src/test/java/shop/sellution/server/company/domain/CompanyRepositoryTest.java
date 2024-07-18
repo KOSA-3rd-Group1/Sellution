0cb6499ac71a556d575b01f6ead42c68910f06d6
김현주 <hyunju0618@naver.com>
2024-07-18 16:25:51 +0900

    chore : merge위한 conflict 해결

--- backend/server/src/test/java/shop/sellution/server/company/domain/CompanyRepositoryTest.java
+++ backend/server/src/test/java/shop/sellution/server/company/domain/CompanyRepositoryTest.java
@@ -20,17 +20,17 @@ class CompanyRepositoryTest {
 
     @DisplayName("name에서 company_id를 추출한다")
     @Test
-    void findCompanyIdByName_found() {
+    void findByName_found() {
         // given
         Company company = new Company();
         company.setName("carrot");
         company.setCompanyId(10L);
         companyRepository.save(company);
 
-        when(companyRepository.findCompanyIdByName("carrot")).thenReturn(Optional.of(company));
+        when(companyRepository.findByName("carrot")).thenReturn(Optional.of(company));
 
         // when
-        Optional<Company> foundCompany = companyRepository.findCompanyIdByName("carrot");
+        Optional<Company> foundCompany = companyRepository.findByName("carrot");
 
         // then
         assertThat(foundCompany).isPresent();
@@ -40,12 +40,12 @@ class CompanyRepositoryTest {
 
     @DisplayName("name에서 company_id를 찾지 못한다")
     @Test
-    void findCompanyIdByName_notFound() {
+    void findByName_notFound() {
         // given
-        when(companyRepository.findCompanyIdByName("carrotttt")).thenReturn(Optional.empty());
+        when(companyRepository.findByName("carrotttt")).thenReturn(Optional.empty());
 
         // when
-        Optional<Company> foundCompany = companyRepository.findCompanyIdByName("carrotttt");
+        Optional<Company> foundCompany = companyRepository.findByName("carrotttt");
 
         // then (Optional 객체가 비어있는지 검증)
         assertThat(foundCompany).isNotPresent();
