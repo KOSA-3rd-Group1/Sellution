b7e149f24aa699636f2ef63181b1fbbb3d14f3e6
김현주 <hyunju0618@naver.com>
2024-07-18 15:22:26 +0900

    [K1P-134] feat: 회원의 카테고리 조회 api 개발

---
+++ backend/server/src/main/java/shop/sellution/server/category/application/CustomerCategoryServiceImpl.java
@@ -0,0 +1,32 @@
+package shop.sellution.server.category.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shop.sellution.server.category.domain.Category;
+import shop.sellution.server.category.domain.CustomerCategoryRepositoryCustom;
+import shop.sellution.server.category.dto.response.FindCustomerCategoryRes;
+import shop.sellution.server.company.domain.CompanyRepository;
+import shop.sellution.server.global.type.DisplayStatus;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CustomerCategoryServiceImpl implements CustomerCategoryService{
+    private final CustomerCategoryRepositoryCustom customerCategoryRepositoryCustom;
+    private final CompanyRepository companyRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<FindCustomerCategoryRes> findAllCategories(String name) {
+        Long companyId = companyRepository.findByName(name).orElseThrow().getCompanyId();
+        List<Category> categories = customerCategoryRepositoryCustom.findAllCategories(companyId, DisplayStatus.Y);
+        return categories.stream()
+                .map(FindCustomerCategoryRes::fromEntity)
+                .toList();
+    }
+
+
+}
