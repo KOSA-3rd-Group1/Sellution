11c283d068be0ca7242fda0f41b94925e9b8000b
김현주 <hyunju0618@naver.com>
2024-07-22 13:21:58 +0900

    [K1P-135, K1P-175] feat: 회원시점 상품 조회 화면 UI 진행중

--- frontend/sellution-app/src/shopping/component/onetimeDelivery/DetailComponent.jsx
+++ frontend/sellution-app/src/shopping/component/onetimeDelivery/DetailComponent.jsx
@@ -1,5 +1,91 @@
+import useDetail from '../../business/onetimeDelivery/useDetail';
+
 const DetailComponent = () => {
-  return <div>단건 주문 상품 상세 페이지</div>;
+  const { activeSlide, setActiveSlide, productToShow, isLoading, error } = useDetail();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!productToShow) {
+    return <div>Product not found</div>;
+  }
+
+  return (
+    <div className='w-full h-full flex justify-center items-center relative'>
+      <main
+        className={`w-10/12 h-[90%] main-box detail-box rounded-lg flex flex-col shadow-md transform`}
+      >
+        {/* carousel start */}
+        <div className='carousel detail-box-1 image-box flex-[4] flex flex-col justify-center items-center w-full'>
+          <div
+            id='slide1'
+            className={`carousel-item relative product-image w-[85%] h-auto aspect-square bg-cover rounded-lg shadow-md ${
+              activeSlide === 1 ? 'block' : 'hidden'
+            }`}
+            style={{ backgroundImage: `url('/image/nike1.png')` }}
+          ></div>
+          <div
+            id='slide2'
+            className={`carousel-item relative product-image w-[85%] h-auto aspect-square bg-cover rounded-lg shadow-md ${
+              activeSlide === 2 ? 'block' : 'hidden'
+            }`}
+            style={{ backgroundImage: `url('/image/nike2.png')` }}
+          ></div>
+          <div
+            id='slide3'
+            className={`carousel-item relative product-image w-[85%] h-auto aspect-square bg-cover rounded-lg shadow-md ${
+              activeSlide === 3 ? 'block' : 'hidden'
+            }`}
+            style={{ backgroundImage: `url('/image/nike3.png')` }}
+          ></div>
+          <div
+            id='slide4'
+            className={`carousel-item relative product-image w-[85%] h-auto aspect-square bg-cover rounded-lg shadow-md ${
+              activeSlide === 4 ? 'block' : 'hidden'
+            }`}
+            style={{ backgroundImage: `url('/image/nike4.png')` }}
+          ></div>
+          {/* carousel button start */}
+          <div className='flex justify-center mt-6 gap-2'>
+            {[1, 2, 3, 4].map((slide) => (
+              <button
+                key={slide}
+                className={`w-2 h-2 rounded-full ${
+                  activeSlide === slide ? 'bg-orange-500' : 'bg-gray-300'
+                }`}
+                onClick={() => setActiveSlide(slide)}
+              ></button>
+            ))}
+          </div>
+          {/* carousel button end */}
+        </div>
+        {/* carousel end */}
+        <div className='detail-box-2 info-box flex-[2.3] flex flex-col p-6'>
+          <span className='flex-[2] info-1 font-bold text-lg'>{productToShow.name}</span>
+          <span className='flex-[2] info-2 text-gray-500'>{productToShow.category}</span>
+          <p className='flex-[5] info-3 text-gray-500 text-sm'>{productToShow.description}</p>
+          <div className='flex-[7] info-4 flex flex-col items-end justify-end mt-auto'>
+            <div className='discount-box flex items-center'>
+              <span className='discount-rate text-xs bg-orange-600 text-white rounded-sm py-1 px-2 mr-2'>
+                {productToShow.discountRate}%
+              </span>
+              <span className='price text-sm text-gray-400 line-through'>
+                {productToShow.price}원
+              </span>
+            </div>
+            <span className='discount-price text-lg text-orange-600 font-bold font-shadow'>
+              {productToShow.discountedPrice}원
+            </span>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default DetailComponent;
--- frontend/sellution-app/src/shopping/component/subscriptionDelivery/ListComponent.jsx
+++ frontend/sellution-app/src/shopping/component/subscriptionDelivery/ListComponent.jsx
@@ -1,5 +1,47 @@
+import useList from '../../business/subscriptionDelivery/useList';
+import OneButtonFooterLayout from '../../layout/OneButtonFooterLayout';
+import MenuCategoryHeaderNav from '../../layout/MenuCategoryHeaderNav';
+import useClientName from '../../business/layout/useClientName';
+import { Link } from 'react-router-dom';
+
 const ListComponent = () => {
-  return <div>정기배송 상품 목록 페이지</div>;
+  const { isListBlurred, subscriptionDeliveryProductList } = useList();
+  const { clientName } = useClientName();
+  //정기배송 상품 더미데이터
+  return (
+    <>
+      <MenuCategoryHeaderNav title={'정기배송 상품 목록'} />
+      <main className={`main-box w-full ${isListBlurred ? 'blurred' : ''}`}>
+        <ul className='product-list w-[90%] mx-auto bg-white list-none p-0'>
+          {subscriptionDeliveryProductList.map((product) => (
+            <Link to={`/shopping/${clientName}/onetime/${product.id}`} key={product.id}>
+              <li
+                key={product.id}
+                className='product-item flex py-4 border-b border-gray-200 '
+                style={{ height: 'calc((100vh - 7.5rem) / 5)' }}
+              >
+                <div className='product-item-1 flex-[3] flex justify-center items-center'>
+                  <div
+                    className='product-image w-full max-w-xs h-auto aspect-square rounded-lg bg-cover'
+                    style={{ backgroundImage: `url('/image/product_img.png')` }}
+                  ></div>
+                </div>
+                <div className='product-item-2 flex-[7] flex flex-col justify-center px-4'>
+                  <div className='product-name font-bold text-sm my-1 flex-[1] flex items-end'>
+                    {product.name}
+                  </div>
+                  <div className='product-price text-orange-500 my-1 flex-[1] flex items-start'>
+                    {product.price}원
+                  </div>
+                </div>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </main>
+      <OneButtonFooterLayout footerText={'장바구니로 이동'} />
+    </>
+  );
 };
 
 export default ListComponent;
