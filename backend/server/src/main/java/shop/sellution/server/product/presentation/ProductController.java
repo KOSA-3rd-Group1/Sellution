f211d08f7b4a683b86d0aabf6f462349c8f97a74
이재아 <jaeah2171@gmail.com>
2024-07-24 15:47:59 +0900

    [K1P-205] feat : 상품 필터링 구현

--- backend/server/src/main/java/shop/sellution/server/product/presentation/ProductController.java
+++ backend/server/src/main/java/shop/sellution/server/product/presentation/ProductController.java
@@ -30,9 +30,14 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity<Page<FindProductRes>> getAllProducts(@RequestParam(defaultValue = "0") int page,
-                                                               @RequestParam(defaultValue = "10") int size) {
+                                                               @RequestParam(defaultValue = "10") int size,
+                                                               @RequestParam(required = false) String deliveryType,
+                                                               @RequestParam(required = false) String isDiscount,
+                                                               @RequestParam(required = false) String categoryName,
+                                                               @RequestParam(required = false) String isVisible,
+                                                               @RequestParam(required = false) String productName) {
         Pageable pageable = PageRequest.of(page, size);
-        return ResponseEntity.ok(productService.getAllProducts(pageable));
+        return ResponseEntity.ok(productService.getAllProducts(pageable, deliveryType, isDiscount, categoryName, isVisible, productName));
     }
 
     @GetMapping("/{productId}")
