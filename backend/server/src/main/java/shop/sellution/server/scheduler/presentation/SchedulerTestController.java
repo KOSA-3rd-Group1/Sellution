290897976e450e16db89d25fe63df76ad754adb9
길재현 <wogus4048@naver.com>
2024-08-01 18:07:36 +0900

    [K1P-269] feat: 서버실행 테스트용 API 추가

--- backend/server/src/main/java/shop/sellution/server/scheduler/presentation/SchedulerTestController.java
+++ backend/server/src/main/java/shop/sellution/server/scheduler/presentation/SchedulerTestController.java
@@ -1,13 +1,14 @@
 package shop.sellution.server.scheduler.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import shop.sellution.server.scheduler.application.SchedulerService;
 
 @RestController
-@RequestMapping("test")
+@RequestMapping("/test")
 @RequiredArgsConstructor
 public class SchedulerTestController {
 
@@ -16,5 +17,10 @@ public class SchedulerTestController {
     @PostMapping("scheduler")
     public void testScheduler() {
         schedulerService.regularProcess();
+    }
+
+    @GetMapping("/running")
+    public String applicationIsRunning() {
+        return "Success Application Running";
     }
 }
