70ae7c988d36a460c011962509905dc281eb695a
길재현 <wogus4048@naver.com>
2024-08-01 22:16:40 +0900

    [K1P-216] feat: 서버실행테스트용 코드 추가

---
+++ backendPG/pgserver/src/main/java/pg/sellution/pgserver/test/TestController.java
@@ -0,0 +1,14 @@
+package pg.sellution.pgserver.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/running")
+    public String applicationIsRunning() {
+        return "Success PG-Application Running";
+    }
+}
