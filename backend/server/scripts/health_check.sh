d236119e9878057cfcb150ede2cbd5fe704d9a39
길재현 <wogus4048@naver.com>
2024-08-02 10:49:37 +0900

    [K1P-269] 인프라 테스트

--- backend/server/scripts/health_check.sh
+++ backend/server/scripts/health_check.sh
@@ -11,13 +11,13 @@ else
     exit 1
 fi
 
-echo "> Start health check of WAS at 'https://127.0.0.1:${TARGET_PORT}' ..."
+echo "> Start health check of WAS at 'http://127.0.0.1:${TARGET_PORT}' ..."
 
 for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10
 do
     echo "> #${RETRY_COUNT} trying..."
 
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  https://127.0.0.1:${TARGET_PORT}/test/running)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  http://127.0.0.1:${TARGET_PORT}/test/running)
 
     if [ ${RESPONSE_CODE} -eq 200 ]; then
         echo "> New WAS successfully running" 
